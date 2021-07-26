010b8d8599e9c6b453ad69404f62cb8414422422
DoodMac <48062579+jbird84@users.noreply.github.com>
2021-07-26 14:55:03 -0500

    Initial Commit

---
+++ profileImage/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  profileImage
+//
+//  Created by Kinney Kare on 7/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ profileImage/profileImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  profileImageApp.swift
+//  profileImage
+//
+//  Created by Kinney Kare on 7/26/21.
+//
+
+import SwiftUI
+
+@main
+struct profileImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
